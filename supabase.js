aa1fe3fb601fa726fa87241ba6e99de9722a99a2
Joker7657 <117198873+Joker7657@users.noreply.github.com>
2026-01-07 15:41:19 +0000

    foto5728

--- supabase.js
+++ supabase.js
@@ -1,5 +1,5 @@
 // supabase.js
-const SUPABASE_URL = "SUA_URL_AQUI";
-const SUPABASE_KEY = "SUA_PUBLIC_ANON_KEY";
+const SUPABASE_URL = "https://jiwkwchkzqtxubvuiqyw.supabase.co";
+const SUPABASE_KEY = "sb_secret__zfPB9z7VfpC6lb9ljDVDA_0k1rQPp3";
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
